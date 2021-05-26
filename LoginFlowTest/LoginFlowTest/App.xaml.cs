91639422b78a8456a35cd613f312891748d95148
Jayson Ragasa <jayson.d.ragasa@live.com>
2021-05-26 14:41:05 +0800

    Login, Registration, Main Page Flow and basic MVVM

---
+++ LoginFlowTest/LoginFlowTest/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using LoginFlowTest.Pages;
+using Xamarin.Forms;
+
+namespace LoginFlowTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            RegisterPage();
+
+            MainPage = new AppShell();
+        }
+
+        void RegisterPage()
+        {
+            // register other pages not part in AppShell
+            Routing.RegisterRoute(nameof(LoginPage) + "/" + nameof(RegistrationPage), typeof(RegistrationPage));
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
