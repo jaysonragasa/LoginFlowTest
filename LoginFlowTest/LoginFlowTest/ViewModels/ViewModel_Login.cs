ebe4189cc7e40ec4ef4dce7301f274c85df234e9
Jayson Ragasa <jayson.d.ragasa@live.com>
2021-05-26 14:46:04 +0800

    add styles

---
+++ LoginFlowTest/LoginFlowTest/ViewModels/ViewModel_Login.cs
@@ -0,0 +1,53 @@
+﻿using LoginFlowTest.Pages;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LoginFlowTest.ViewModels
+{
+    public class ViewModel_Login
+    {
+        #region events
+
+        #endregion
+
+        #region vars
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+        #region commands
+        public ICommand Command_Login { get; set; }
+        public ICommand Command_Register { get; set; }
+        #endregion
+
+        #region ctors
+        public ViewModel_Login()
+        {
+            InitCommands();
+        }
+        #endregion
+
+        #region command methods
+        async void Command_Login_Click()
+        {
+            await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
+        }
+
+        async void Command_Register_Click()
+        {
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}/{nameof(RegistrationPage)}");
+        }
+        #endregion
+
+        #region methods
+        void InitCommands()
+        {
+            if (Command_Login == null) Command_Login = new Command(Command_Login_Click);
+            if (Command_Register == null) Command_Register = new Command(Command_Register_Click);
+        }
+        #endregion
+    }
+}
---
+++ LoginFlowTest/LoginFlowTest/ViewModels/ViewModel_Logout.cs
@@ -0,0 +1,46 @@
+﻿using LoginFlowTest.Pages;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LoginFlowTest.ViewModels
+{
+    public class ViewModel_Logout
+    {
+        #region events
+
+        #endregion
+
+        #region vars
+
+        #endregion
+
+        #region properties
+        public ICommand Command_Logout { get; set; }
+        #endregion
+
+        #region commands
+
+        #endregion
+
+        #region ctors
+        public ViewModel_Logout()
+        {
+            InitCommands();
+        }
+        #endregion
+
+        #region command methods
+        void Command_Logout_Click()
+        {
+            Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+        #endregion
+
+        #region methods
+        void InitCommands()
+        {
+            if (Command_Logout == null) Command_Logout = new Command(Command_Logout_Click);
+        }
+        #endregion
+    }
+}
